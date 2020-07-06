adc6f9f914795ea3243ac0ffbf37990f0c3e9fe1
charlyTochi <orafu@gmail.com>
2020-07-06 03:02:43 +0100

    Updates

---
+++ precache-manifest.5acf80b8431879f961c346cd63cdd264.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba3aad673160a5d2e6dc9e909f1b60c1",
+    "url": "/school/webapp/index.html"
+  },
+  {
+    "revision": "4053d5d76d4caf100197",
+    "url": "/school/webapp/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "1d8fbab8bfff3dc61488",
+    "url": "/school/webapp/static/css/main.173f9e75.chunk.css"
+  },
+  {
+    "revision": "4053d5d76d4caf100197",
+    "url": "/school/webapp/static/js/2.b8f7cfbc.chunk.js"
+  },
+  {
+    "revision": "9c4a88d9b57cf919f2c23e04e5eabae2",
+    "url": "/school/webapp/static/js/2.b8f7cfbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d8fbab8bfff3dc61488",
+    "url": "/school/webapp/static/js/main.7dbb1c60.chunk.js"
+  },
+  {
+    "revision": "c5eafabe4b2314660f3de33fe2c9484b",
+    "url": "/school/webapp/static/js/main.7dbb1c60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9afec793b2c9a60eb449",
+    "url": "/school/webapp/static/js/runtime-main.4049993f.js"
+  },
+  {
+    "revision": "056eb0586182a8d28273ad11c028e912",
+    "url": "/school/webapp/static/media/bg5.056eb058.jpg"
+  },
+  {
+    "revision": "eec7c7f60134e712ef7174c96ca7ee5a",
+    "url": "/school/webapp/static/media/logo-white.eec7c7f6.svg"
+  },
+  {
+    "revision": "aab414f7b69d0d4ad897e75e735f240a",
+    "url": "/school/webapp/static/media/mike.aab414f7.jpg"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/school/webapp/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/school/webapp/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/school/webapp/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/school/webapp/static/media/nucleo-outline.8ebec31f.woff2"
+  }
+]);
